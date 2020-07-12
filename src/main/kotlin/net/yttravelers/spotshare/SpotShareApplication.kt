f7292caabd3e1d388f0c9f3f3d010586ea4712a2
Nolit <yaboonjapan@gmail.com>
2020-07-12 16:06:21 +0900

    initial commit

---
+++ src/main/kotlin/net/yttravelers/spotshare/SpotShareApplication.kt
@@ -0,0 +1,11 @@
+package net.yttravelers.spotshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotShareApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotShareApplication>(*args)
+}
---
+++ src/test/kotlin/net/yttravelers/spotshare/SpotShareApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.yttravelers.spotshare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotShareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
