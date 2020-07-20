90eea4041d9dd5a509ad8419ce30543626eb63b6
水薮 優馬 <y_mizuyabu@kingprinter.com>
2020-07-20 18:23:01 +0900

    スポットの一覧表示・登録・更新・詳細のAPIを作成

---
+++ src/main/kotlin/net/yttravelers/spotshare/controllers/SpotController.kt
@@ -0,0 +1,57 @@
+package net.yttravelers.spotshare.controllers
+
+import net.yttravelers.spotshare.entities.Spot
+import net.yttravelers.spotshare.services.SpotService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+class SpotForm() {
+    var routeNumber: Int? = null
+
+    var scheduleDateTime: LocalDateTime? = null
+
+    var country: String? = null
+
+    var spotName: String? = null
+
+    var costExpectation: Int? = null
+
+    var requiredTimeExpectation: Int? = null
+
+    var favoritePoint: Int? = null
+
+    var url: String? = null
+
+    var remark: String = ""
+
+    //TODO: 画像を追加
+    //TODO: 座標を追加
+}
+
+@RestController
+@RequestMapping("/spots")
+class SpotController {
+    @Autowired
+    lateinit var spotService: SpotService
+
+    @GetMapping
+    fun index(): List<Spot> {
+        return spotService.getAll()
+    }
+
+    @PostMapping
+    fun addSpot(@RequestBody spot: SpotForm): Spot {
+        return this.spotService.addSpot(spot)
+    }
+
+    @PatchMapping("/{id}")
+    fun updateSpot(@PathVariable("id") id: Int, @RequestBody spot: SpotForm): Spot {
+        return this.spotService.updateSpot(id, spot)
+    }
+
+    @GetMapping("/{id}")
+    fun findSpot(@PathVariable("id") id: Int): Spot {
+        return spotService.findSpotOrException(id)
+    }
+}
