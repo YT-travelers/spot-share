2e58671420efc5ca30aaaa7a5c668db834e790fc
大脇泰良 <b3256@oic.jp>
2020-09-14 20:44:38 +0900

    routeエンティティとサービス追加

---
+++ src/main/angular/src/app/entity/route.ts
@@ -0,0 +1,8 @@
+export interface Route {
+  /** id */
+  id: number;
+  /** ルートタイトル */
+  routeTitle: string;
+  /** スポット番号 */
+  spotid: number;
+}
---
+++ src/main/angular/src/app/shared/route.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Route } from '../entity/route';
+
+@Injectable({ providedIn: 'root' })
+export class RouteService {
+
+  private url = 'api/route'
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   * ルート新規作成
+   * @param route ルート情報
+   */
+  createRoute(spots: number[]): Observable<Route> {
+    return this.http.post<Route>(this.url, spots)
+      .pipe(
+        catchError(this.handleError<Route>(null))
+      );
+  }
+
+  /**
+   * ルート更新
+   * @param route ルート情報
+   */
+  updateRoute(spots: number[], id: number): Observable<Route> {
+    return this.http.patch<Route>(this.url + '/' + id, spots)
+      .pipe(
+        catchError(this.handleError<Route>(null))
+      );
+  }
+
+  /**
+   * ルート削除
+   * @param id ルート番号
+   */
+  deleteRoute(id: number): Observable<Route> {
+    return this.http.delete<Route>(this.url + '/' + id)
+      .pipe(
+        catchError(this.handleError<Route>(null))
+      );
+  }
+
+  /**
+   * ルート取得
+   * @param id ルート番号
+   */
+  getRoute(id: number): Observable<Route> {
+    return this.http.get<Route>(this.url + '/' + id)
+      .pipe(
+        catchError(this.handleError<Route>(null))
+      );
+  }
+
+  /**
+   * エラーハンドリング
+   * @param operation - 失敗した操作の名前
+   * @param result - observableな結果として返す任意の値
+   */
+  private handleError<T>(result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+}
