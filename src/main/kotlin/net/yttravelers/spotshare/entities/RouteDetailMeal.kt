ca6f1ff513720f97fd0b3be0dd941009ac5d0796
Nolit <yaboonjapan@gmail.com>
2020-12-02 22:01:41 +0900

    Mealエンティティの作成

---
+++ src/main/kotlin/net/yttravelers/spotshare/entities/RouteDetailMeal.kt
@@ -0,0 +1,25 @@
+package net.yttravelers.spotshare.entities
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import javax.persistence.Table
+import javax.persistence.*
+
+@Entity
+@Table(name = "routeDetailMeal")
+class RouteDetailMeal {
+    @Id
+    var routeDetailId: Int? = null
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "routeDetailId")
+    @MapsId
+    lateinit var routeDetail: RouteDetail
+
+    var mealMinutes: Int? = null
+
+    //TODO: 区分の設計
+//    var mealKindDiv:
+    //必要？
+//    var mealKindDivName:
+}
