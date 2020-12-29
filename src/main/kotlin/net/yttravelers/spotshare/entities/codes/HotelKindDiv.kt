6c6208e8c6015b960205b1e652c4750d41412902
Nolit <yaboonjapan@gmail.com>
2020-12-29 11:06:53 +0900

    (#36): Laravelプロジェクトの作成

--- src/main/kotlin/net/yttravelers/spotshare/entities/codes/HotelKindDiv.kt
+++
@@ -1,9 +0,0 @@
-package net.yttravelers.spotshare.entities.codes
-
-import org.hibernate.annotations.Where
-import javax.persistence.Entity
-
-@Entity
-@Where(clause = "division='HotelKindDiv'")
-class HotelKindDiv: Code() {
-}
--- src/main/kotlin/net/yttravelers/spotshare/repositories/RouteDetailRepository.kt
+++
@@ -1,7 +0,0 @@
-//package net.yttravelers.spotshare.repositories
-//
-//import net.yttravelers.spotshare.entities.RouteDetail
-//import org.springframework.data.jpa.repository.JpaRepository
-//
-//interface RouteDetailRepository: JpaRepository<RouteDetail, Int> {
-//}
