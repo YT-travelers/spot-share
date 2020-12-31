83734843d9cc4f9136f49226876263dc444a6d70
Nolit <yaboonjapan@gmail.com>
2020-12-31 16:41:11 +0900

    (#36): Activityのモデル作成

---
+++ app/Models/RouteDetailActivity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RouteDetailActivity extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'route_detail_id';
+}
---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Activity;
+use App\Models\Code\CheckDiv;
+use App\Models\RouteDetailActivity;
+use App\Models\RouteDetailChecklist;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ActivityFactory extends Factory
+{
+    use SetRouteDetailId;
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Activity::class;
+
+    /**
+     * Define the model's default state.
+     * activitiesテーブルにデータが入ってることを前提にしてます
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'activity_name' => $this->faker->word,
+            'activity_start_time' => $this->faker->dateTime,
+            'activity_end_time' => $this->faker->dateTime,
+            'activity_summary' => $this->faker->sentence,
+            'activity_address' => $this->faker->address,
+            'activity_url' => $this->faker->url,
+        ];
+    }
+}
