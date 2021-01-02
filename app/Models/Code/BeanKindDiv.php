7249a6e917c8394536015b86258c2dc900a1f28c
Nolit <yaboonjapan@gmail.com>
2021-01-02 23:10:02 +0900

    (#36): BeanKindDivに判定用メソッドを追加

--- app/Models/Code/BeanKindDiv.php
+++ app/Models/Code/BeanKindDiv.php
@@ -15,4 +15,50 @@ class BeanKindDiv extends Code
     const Memo_DIV_VALUE = 8;
 
     protected static $division = 'BeanKindDiv';
+
+    public function isTourism(): bool
+    {
+        return $this->div_value === 0;
+    }
+
+    public function isRestaurant(): bool
+    {
+        return $this->div_value === 1;
+    }
+
+    public function isHotel(): bool
+    {
+        return $this->div_value === 2;
+    }
+
+    public function isActivity(): bool
+    {
+        return $this->div_value === 3;
+    }
+
+    public function isMeal(): bool
+    {
+        return $this->div_value === 4;
+    }
+
+    public function isMove(): bool
+    {
+        return $this->div_value === 5;
+    }
+
+    public function isTime(): bool
+    {
+        return $this->div_value === 6;
+    }
+
+    public function isChecklist(): bool
+    {
+        return $this->div_value === 7;
+    }
+
+    public function isMemo(): bool
+    {
+        return $this->div_value === 8;
+    }
+
 }
