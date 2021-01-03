384a2097ca3944caa2ca03f34eef45af4c3d42d2
Nolit <yaboonjapan@gmail.com>
2021-01-03 17:11:32 +0900

    (#36): ルート削除APIの作成

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RouteRequest;
 use App\Http\Services\RouteService;
 use App\Models\Route;
+use Illuminate\Http\Response;
 
 class RouteController extends Controller
 {
@@ -35,5 +36,16 @@ class RouteController extends Controller
     {
         $route = $routeService->saveRoute($request->validated(), $routeId);
         return $converter->convertRoute($route);
+    }
+
+    /**
+     * @param int $routeId
+     * @param RouteService $routeService
+     * @return Response
+     */
+    public function destroy(int $routeId, RouteService $routeService): Response
+    {
+        $routeService->deleteRoute($routeId);
+        return response(['result' => 'success']);
     }
 }
