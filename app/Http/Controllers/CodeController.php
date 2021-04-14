78d5e5a08470f740919b01b3a7995c57423db63b
Nolit <yaboonjapan@gmail.com>
2021-04-14 21:19:42 +0900

    (#44): CodeControllerがCrudControllerを継承するように修正

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -3,26 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Code\Code;
+use JetBrains\PhpStorm\Pure;
 
-class CodeController extends Controller
+class CodeController extends CrudController
 {
-    public function __construct(private Code $codeModel)
+    #[Pure] public function __construct(private Code $codeModel)
     {
-    }
-
-    public function index(): array
-    {
-        return $this->codeModel
-            ->orderBy('division')
-            ->orderBy('div_value')
-            ->get()->map(function (Code $code) {
-                return [
-                    'division' => $code->division,
-                    'divKey' => $code->div_key,
-                    'divName' => $code->div_name,
-                    'divKeyName' => $code->div_key_name,
-                    'divValue' => $code->div_value,
-                ];
-            })->toArray();
+        parent::__construct($this->codeModel);
     }
 }
