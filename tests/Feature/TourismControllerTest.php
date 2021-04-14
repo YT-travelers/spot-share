16a233781a37c4a6e496075f0e9be97d8a62987b
Nolit <yaboonjapan@gmail.com>
2021-04-14 20:41:03 +0900

    (#43): マスタ情報のCRUDのテストコード作成

--- tests/Feature/TourismControllerTest.php
+++ tests/Feature/TourismControllerTest.php
@@ -2,10 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tourism;
 use Tests\TestCase;
 
 class TourismControllerTest extends TestCase
 {
+    private const TOURISM_STRUCTURE = [
+        'tourismId',
+        'tourismName',
+        'country',
+        'tourismOpenTime',
+        'tourismCloseTime',
+        'tourismSummary',
+        'tourismAddress',
+        'tourismUrl'
+    ];
     /**
      * A basic feature test example.
      *
@@ -15,15 +26,40 @@ class TourismControllerTest extends TestCase
     {
         $response = $this->get('/tourisms');
 
-        $response->assertJsonStructure([[
-            'tourismId',
-            'tourismName',
-            'country',
-            'tourismOpenTime',
-            'tourismCloseTime',
-            'tourismSummary',
-            'tourismAddress',
-            'tourismUrl'
-        ]]);
+        $response->assertJsonStructure([self::TOURISM_STRUCTURE]);
+    }
+
+    public function testStore()
+    {
+        $postData = Tourism::factory()->make()->toArray();
+        $response = $this->post('tourisms', $postData);
+
+        $response->assertJsonStructure(self::TOURISM_STRUCTURE);
+    }
+
+    public function testShow()
+    {
+        $tourismId = Tourism::first()->tourism_id;
+        $response = $this->get("/tourisms/$tourismId");
+
+        $response->assertJsonStructure(self::TOURISM_STRUCTURE);
+    }
+
+    public function testUpdate()
+    {
+        $updateData = Tourism::factory()->make()->toArray();
+        $tourismId = Tourism::first()->tourism_id;
+        $response = $this->put("/tourisms/$tourismId", $updateData);
+
+        $response->assertJsonStructure(self::TOURISM_STRUCTURE);
+    }
+
+    public function testDestroy()
+    {
+        //NOTE: 外部キー制約があるので、このクラスのtestStoreで作成したレコードに対してテストを行う
+        $tourismId = Tourism::latest()->first()->tourism_id;
+        $response = $this->delete("tourisms/$tourismId");
+
+        $response->assertJsonStructure(self::TOURISM_STRUCTURE);
     }
 }
