3484ab94366e0c1c281c507584f8aa72f7a1b717
Taira <b3256@oic.jp>
2021-05-02 22:33:02 +0900

    Utilと定数クラス追加

---
+++ src/main/angular/src/app/shared/const.const.ts
@@ -0,0 +1,12 @@
+
+/**
+ * 正規表現
+ */
+export namespace Const.RegularExpr {
+  /** 数字 */
+  export const Number = '^[0-9０-９]*$';
+  /** 半角数字 */
+  export const HalfNumber = '^[0-9]*$';
+  /** 全角数字 */
+  export const FullNumber = '^[０-９]*$';
+}
