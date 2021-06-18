dc9ee4ec97d4842845f41bd9450a5181df83720e
Taira <b3256@oic.jp>
2021-06-18 22:29:38 +0900

    時間ビーン完成

--- src/main/angular/src/app/route-detail-bean/route-detail-time-bean/route-detail-time-bean.component.ts
+++ src/main/angular/src/app/route-detail-bean/route-detail-time-bean/route-detail-time-bean.component.ts
@@ -1,6 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import * as moment from 'moment';
+import { defer as _defer } from 'lodash';
+import { Const } from 'src/app/shared/const/const.const';
 import { IRouteDetailTime } from 'src/app/shared/model/route-detail-time';
+import { TimeUtils } from 'src/app/shared/utils/time-utils.const';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-route-detail-time-bean',
@@ -9,7 +13,7 @@ import { IRouteDetailTime } from 'src/app/shared/model/route-detail-time';
 })
 export class RouteDetailTimeBeanComponent implements OnInit {
 
-  /** ルート詳細移動 */
+  /** ルート詳細時間 */
   @Input() detail: IRouteDetailTime;
 
   /** ルート詳細更新イベント通知 */
@@ -18,13 +22,14 @@ export class RouteDetailTimeBeanComponent implements OnInit {
   /** ビーン削除イベント通知 */
   @Output() deleteRouteDetailTimeEvent: EventEmitter<number> = new EventEmitter();
 
-  /** ルート詳細メモ情報 フォームグループ */
-  routeDetailTimeFormGroup = new FormGroup({
-    /** ルート詳細ID */
-    routeDetailId: new FormControl(0),
-    /** メモ内容 */
-    scheduleDateTime: new FormControl('')
-  });
+  /** 画面編集用 */
+  scheduleDateTime = {
+    year: 0,
+    month: 0,
+    day: 0,
+    hour: 0,
+    minutes: 0,
+  };
 
   constructor(
   ) { }
@@ -34,29 +39,89 @@ export class RouteDetailTimeBeanComponent implements OnInit {
 
   ngOnInit(): void {
     // 入力項目 初期値設定
-    if (!this.detail.scheduleDateTime) {
-      this.detail.scheduleDateTime = '';
+    let dateTime;
+    if (this.detail.scheduleDateTime) {
+      // utcで受け取らないと、アクセス元のブラウザの時差分ずれる
+      dateTime = moment.utc(this.detail.scheduleDateTime).format(Const.DateTimeFormat.Hyphen);
+    } else {
+      // 1.初期値設定の場合はブラウザ依存の時刻を設定すればよいため、utcにはしない
+      // 2.momentでシステム日時を取得した場合、0数えで前月が表示されるためプラス1月して調整する
+      dateTime = moment().add(1, 'month');
     }
 
-    this.routeDetailTimeFormGroup.patchValue(this.detail);
+    this.scheduleDateTime.year = moment(dateTime).year();
+    this.scheduleDateTime.month = moment(dateTime).month();
+    this.scheduleDateTime.day = moment(dateTime).date();
+    this.scheduleDateTime.hour = moment(dateTime).hour();
+    this.scheduleDateTime.minutes = moment(dateTime).minutes();
 
-    // 入力値変更検知
-    this.routeDetailTimeFormGroup.valueChanges.subscribe(() => {
-      this.detail = this.routeDetailTimeFormGroup.value;
+    // 設定した初期値を変更イベントとして通知する
+    this.emitChangeEvent();
 
-      /** ルート詳細更新イベント通知 */
-      this.updateRouteDetailTimeEvent.emit(this.detail);
-    });
   }
 
   // -----------------------------------------------------------------------
   // イベント
 
   /**
+   * 時間変更イベント
+   * @param event 
+   */
+  onChangeHours(event) {
+    this.scheduleDateTime.hour = Number(TimeUtils.complementHour(event));
+    this.emitChangeEvent();
+  }
+
+  /**
+   * 分変更イベント
+   * @param event 
+   */
+  onChangeMinutes(event) {
+    this.scheduleDateTime.minutes = Number(TimeUtils.complementMinutes(event));
+    this.emitChangeEvent();
+  }
+
+  /**
+   * カレンダー日付選択イベント（ngbDatepicker）
+   * @param event 
+   */
+  onDateSelection(event: NgbDateStruct) {
+    this.scheduleDateTime.year = event.year;
+    this.scheduleDateTime.month = event.month;
+    this.scheduleDateTime.day = event.day;
+    this.emitChangeEvent();
+  }
+
+  /**
    * 削除ボタン押下イベント
    */
   onClickDeleteButton(): void {
     this.deleteRouteDetailTimeEvent.emit(this.detail.routeDetailId);
+  }
+
+  // -----------------------------------------------------------------------
+  // 処理
+
+  /**
+   * スケジュール日時変更イベント通知
+   */
+   emitChangeEvent() {
+    const routeDetailTime: IRouteDetailTime = {
+      routeDetailId: this.detail.routeDetailId,
+      scheduleDateTime: moment(this.scheduleDateTime).format(Const.DateTimeFormat.Hyphen),
+    }
+
+    this.updateRouteDetailTimeEvent.emit(routeDetailTime);
+   }
+
+  // -----------------------------------------------------------------------
+  // getter
+
+  /** 編集対象の日付文字列を画面表示用に変換 */
+  get dispScheduleDateTime(): string {
+    // bootstrapのngbDatepickerが月を0数えしているせいか、引数の次月が表示されてしまうため内部的にマイナス1月をして表示を調整する
+    const date = moment.utc(this.scheduleDateTime).subtract(1, 'month');
+    return date.format(Const.DateTimeFormat.SlashMonthDay);
   }
 
 }
--- src/main/angular/src/app/shared/const/const.const.ts
+++ src/main/angular/src/app/shared/const/const.const.ts
@@ -10,3 +10,14 @@ export namespace Const.RegularExpr {
   /** 全角数字 */
   export const FullNumber = '^[０-９]*$';
 }
+
+export namespace Const.DateTimeFormat {
+  /** ハイフン区切り */
+  export const Hyphen = 'YYYY-MM-DD HH:mm:00';
+  /** スラッシュ区切り */
+  export const Slash = 'YYYY/MM/DD HH:mm:00';
+  /** 日本語区切り */
+  export const Japan = 'YYYY年MM月DD日 HH:mm:00';
+  /** 月/日 */
+  export const SlashMonthDay = "MM/DD";
+}
