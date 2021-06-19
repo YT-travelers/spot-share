4c77ac3070588fa1d7faaa485b0692d6ac70b2a7
Nolit <yaboonjapan@gmail.com>
2021-06-19 20:38:59 +0900

    (#78): uploadFilesがnullの場合に空の配列として扱うように修正

--- app/Http/Controllers/TourismController.php
+++ app/Http/Controllers/TourismController.php
@@ -41,7 +41,7 @@ class TourismController extends CrudController
      */
     private function storeOrUpdate(Request $request, int $id = null): JsonResponse
     {
-        $uploadFiles = $request->get('uploadFiles', []);
+        $uploadFiles = $request->get('uploadFiles', []) ?? [];
         $tourismImages = makeArraySnakeRecursively($request->get('tourismImages', []));
         $tourismData = makeArraySnakeRecursively($request->except(['tourismImages', 'uploadFiles']));
         $tourismData['country_code'] = $tourismData['country_code'] ?? $tourismData['country']['country_code'];
